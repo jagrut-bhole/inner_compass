1692aedc7c265f4e0a6ef7ebc621b059af9bc473
tusharchawre <tusharchawre0240@gmail.com>
2024-09-28 16:21:58 +0530

    Blogs

--- controller/authController.js
+++ controller/authController.js
@@ -56,6 +56,7 @@ module.exports.signup_post = async (req, res) => {
     const token = createToken(user._id);
     res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({ user: user._id });
+
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
