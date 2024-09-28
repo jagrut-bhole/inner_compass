92eb23172b23823dab73dc4206d9375243ac64a5
jagrut-bhole <jagrutbhole75@gmail.com>
2024-09-28 17:47:52 +0530

    result done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const blogSchema = require("../models/blogs");
-const questionSchema =  require("../models/question");
+const questionSchema = require("../models/question");
 
 const router = express.Router();
 
@@ -8,17 +8,15 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
-
-router.get("/blogs",(req,res) =>{
+router.get("/blogs", (req, res) => {
   res.render("blogs");
-})
+});
 
-router.get("/api/blogs",async (req,res) =>{
-  const blogs = await blogSchema.find()
-  
-  res.json(blogs)
-})
+router.get("/api/blogs", async (req, res) => {
+  const blogs = await blogSchema.find();
 
+  res.json(blogs);
+});
 
 router.get("/api/blogs/:id", async (req, res) => {
   const blogs = await blogSchema.findById(req.params.id);
@@ -45,18 +43,13 @@ router.get("/blogs/:id", (req, res) => {
   res.render("blog-details", { blogId });
 });
 
-router.get("/result", (req, res) => {
-  const resultMessage = req.query.message;
-  res.render("result", { resultMessage });
-});
-
 // Question section
 
-router.get("/questions",(req,res) =>{
+router.get("/questions", (req, res) => {
   res.render("questionnaire");
 });
 
-router.get("/api/questions", async (req,res) => {
+router.get("/api/questions", async (req, res) => {
   const questions = await questionSchema.find();
 
   res.json(questions);
@@ -97,10 +90,13 @@ router.post("/api/submit-quiz", (req, res) => {
 });
 
 router.get("/result", (req, res) => {
+  
+  console.log("Session resultMessage:", req.session.resultMessage);
+
   const resultMessage = req.session.resultMessage;
 
   if (!resultMessage) {
-    res.redirect("/questions");
+    return res.redirect("/questions");
   }
 
   res.render("result", { resultMessage });
@@ -116,4 +112,3 @@ function calculateScore(answers) {
 }
 
 module.exports = router;
- 
